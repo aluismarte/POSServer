e38afd6445d9819704223efacf5448c2e0a772ee
Aluis Marte <aluismarte@gmail.com>
2019-11-02 18:09:10 -0400

    Ahora el bootstrap se ejecuta completo por el momento.

--- src/main/java/com/alsjava/courses/servers/utils/bootstrap/BootStrap.java
+++ src/main/java/com/alsjava/courses/servers/utils/bootstrap/BootStrap.java
@@ -1,5 +1,7 @@
 package com.alsjava.courses.servers.utils.bootstrap;
 
+import com.alsjava.courses.servers.utils.bootstrap.control.BasicProducts;
+import com.alsjava.courses.servers.utils.bootstrap.security.BasicTerminals;
 import com.alsjava.courses.servers.utils.bootstrap.security.BasicUsers;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +12,16 @@ import org.springframework.stereotype.Component;
 public class BootStrap {
 
     private BasicUsers basicUsers = new BasicUsers();
+    private BasicTerminals basicTerminals = new BasicTerminals();
+    private BasicProducts basicProducts = new BasicProducts();
 
     public BootStrap() {
     }
 
     public void init() {
         basicUsers.insert();
+        basicTerminals.insert();
+        basicProducts.insert();
         System.out.println("Finish Bootstrap load");
     }
 
