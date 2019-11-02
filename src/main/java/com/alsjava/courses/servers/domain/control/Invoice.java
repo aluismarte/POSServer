d1b5b0860965dd578f4fae718840f521dc6cdc83
Aluis Marte <aluismarte@gmail.com>
2019-11-02 18:02:30 -0400

    Ajustando la DB.

--- src/main/java/com/alsjava/courses/servers/domain/control/Invoice.java
+++ src/main/java/com/alsjava/courses/servers/domain/control/Invoice.java
@@ -1,11 +1,13 @@
 package com.alsjava.courses.servers.domain.control;
 
 import com.alsjava.courses.servers.domain.AuditDomain;
+import com.alsjava.courses.servers.domain.security.Terminal;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Created by aluis on 11/2/19.
@@ -15,4 +17,17 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "invoices")
 public class Invoice extends AuditDomain {
+
+    @Column
+    private Terminal terminal;
+
+    @Column
+    private String serial;
+
+    @Column
+    private LocalDateTime saleDateTime;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @CollectionTable(name = "invoices_invoiceDetails")
+    private List<InvoiceDetail> invoiceDetails;
 }
---
+++ src/main/java/com/alsjava/courses/servers/domain/control/InvoiceDetail.java
@@ -0,0 +1,28 @@
+package com.alsjava.courses.servers.domain.control;
+
+import com.alsjava.courses.servers.domain.AuditDomain;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by aluis on 11/2/19.
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
+@Table(name = "invoice_details")
+public class InvoiceDetail extends AuditDomain {
+
+    @Column
+    private Product product;
+
+    @Column
+    private long quantity = 0L;
+
+    @Column
+    private double price = 0;
+}
