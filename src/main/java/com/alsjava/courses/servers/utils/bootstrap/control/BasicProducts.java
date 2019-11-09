84a6d2cdb86440a6d33adbc3903784eb13ceab15
Aluis Marte <aluismarte@gmail.com>
2019-11-09 16:08:28 -0400

    Adicionandno mas productos.

--- src/main/java/com/alsjava/courses/servers/utils/bootstrap/control/BasicProducts.java
+++ src/main/java/com/alsjava/courses/servers/utils/bootstrap/control/BasicProducts.java
@@ -23,6 +23,10 @@ public class BasicProducts implements BootStrapInsert {
         create("Limón", 20, 10, "https://media.metrolatam.com/2019/07/01/dietalimn-65c34166ec988a6ae136e7cbbdea9f80-600x400.jpg");
         create("Naranja", 100, 10, "https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/201811/26/00118105700068____2__600x600.jpg");
         create("Martillo", 0, 250, "https://d2mq510qed945.cloudfront.net/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/S/T/STAN0475.jpg");
+        create("Uva", 2000, 5, "https://comefruta.es/wp-content/uploads/2013/11/uva-roja.jpg");
+        create("Mesa", 10, 5000, "https://mesasdemadera.org/wp-content/uploads/2019/05/mesa-de-madera-de-pino-dogar-kynus.jpg");
+        create("Laptop", 10, 70000, "https://media.4rgos.it/i/Argos/8684576_R_Z001A?w=750&h=440&qlt=70");
+        create("Manzana", 235, 50, "https://cdn.tn.com.ar/sites/default/files/styles/1366x765/public/2019/08/05/diseno_sin_titulo_25.jpg");
     }
 
     private Product create(String name, long quantity, double price, String image) {
